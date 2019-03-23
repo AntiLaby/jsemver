6e57cfee3faf0ec40df68fc0616543f5bcd60860
heisluft <heisluftlp@gmail.com>
2019-03-23 22:49:23 +0100

    implement Maven version ranges pt 1

--- src/main/java/com/github/zafarkhaja/semver/expr/MvnLexer.java
+++ src/main/java/com/github/zafarkhaja/semver/expr/MvnLexer.java
@@ -1,6 +1,8 @@
-package com.github.zafarkhaja.semver.maven;
+package com.github.zafarkhaja.semver.expr;
 
+import com.github.zafarkhaja.semver.compiling.Lexer;
 import com.github.zafarkhaja.semver.compiling.LexerException;
+import com.github.zafarkhaja.semver.compiling.Token;
 import com.github.zafarkhaja.semver.util.Stream;
 
 import java.util.ArrayList;
@@ -8,18 +10,18 @@ import java.util.List;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
-public class Lexer {
-    Stream<Lexer.Token> tokenize(String input) {
-        List<Token> tokens = new ArrayList<>();
+class MvnLexer extends Lexer<MvnLexer.MvnToken> {
+    protected Stream<MvnToken> tokenize(String input) {
+        List<MvnToken> tokens = new ArrayList<>();
         int tokenPos = 0;
         while (!input.isEmpty()) {
             boolean matched = false;
-            for (Token.Type tokenType : Token.Type.values()) {
+            for (MvnToken.Type tokenType : MvnToken.Type.values()) {
                 Matcher matcher = tokenType.pattern.matcher(input);
                 if (matcher.find()) {
                     matched = true;
                     input = matcher.replaceFirst("");
-                    tokens.add(new Token(tokenType, matcher.group(), tokenPos));
+                    tokens.add(new MvnToken(tokenType, matcher.group(), tokenPos));
                     tokenPos += matcher.end();
                     break;
                 }
@@ -28,24 +30,11 @@ public class Lexer {
                 throw new LexerException(input);
             }
         }
-        tokens.add(new Token(Token.Type.EOI, null, tokenPos));
-        return new Stream<>(tokens.toArray(new Token[tokens.size()]));
+        tokens.add(new MvnToken(MvnToken.Type.EOI, null, tokenPos));
+        return new Stream<>(tokens.toArray(new MvnToken[tokens.size()]));
     }
 
-    static class Token {
-        /**
-         * The type of this token.
-         */
-        final Lexer.Token.Type type;
-        /**
-         * The lexeme of this token.
-         */
-        final String lexeme;
-        /**
-         * The position of this token.
-         */
-        final int position;
-
+    static class MvnToken extends Token<MvnToken> {
         /**
          * Constructs a {@code Token} instance with the type, lexeme and position.
          *
@@ -53,53 +42,16 @@ public class Lexer {
          * @param lexeme   the lexeme of this token
          * @param position the position of this token
          */
-        Token(Lexer.Token.Type type, String lexeme, int position) {
-            this.type = type;
-            this.lexeme = (lexeme == null) ? "" : lexeme;
-            this.position = position;
+        MvnToken(MvnToken.Type type, String lexeme, int position) {
+            super(type, (lexeme == null) ? "" : lexeme, position);
         }
 
-        /**
-         * {@inheritDoc}
-         */
-        @Override
-        public boolean equals(Object other) {
-            if (this == other) {
-                return true;
-            }
-            if (!(other instanceof Lexer.Token)) {
-                return false;
-            }
-            Lexer.Token token = (Lexer.Token) other;
-            return type.equals(token.type) && lexeme.equals(token.lexeme) && position == token.position;
-        }
-
-        /**
-         * {@inheritDoc}
-         */
-        @Override
-        public int hashCode() {
-            int hash = 5;
-            hash = 71 * hash + type.hashCode();
-            hash = 71 * hash + lexeme.hashCode();
-            hash = 71 * hash + position;
-            return hash;
-        }
-
-        /**
-         * Returns the string representation of this token.
-         *
-         * @return the string representation of this token
-         */
-        @Override
-        public String toString() {
-            return String.format("%s(%s) at position %d", type.name(), lexeme, position);
-        }
-
-        enum Type implements Stream.ElementType<Token> {
+        enum Type implements Token.Type<MvnToken> {
+            ALPHA_NUMERIC("(\\d*\\p{L}(\\p{L}|\\d)*)|0\\d+"),
             NUMERIC("0|[1-9][0-9]*"),
             DOT("\\."),
             HYPHEN("-"),
+            PLUS("\\+"),
             LEFT_SQBR("\\["),
             RIGHT_SQBR("\\]"),
             LEFT_PAREN("\\("),
@@ -136,7 +88,7 @@ public class Lexer {
              * {@inheritDoc}
              */
             @Override
-            public boolean isMatchedBy(Token token) {
+            public boolean isMatchedBy(MvnToken token) {
                 return token != null && this == token.type;
             }
         }
---
+++ src/main/java/com/github/zafarkhaja/semver/expr/RuleSet.java
@@ -0,0 +1,82 @@
+package com.github.zafarkhaja.semver.expr;
+
+import com.github.zafarkhaja.semver.Parser;
+import com.github.zafarkhaja.semver.Version;
+import com.github.zafarkhaja.semver.compiling.UnexpectedTokenException;
+import com.github.zafarkhaja.semver.util.Stream;
+
+import java.util.function.Predicate;
+
+import static com.github.zafarkhaja.semver.expr.MvnLexer.MvnToken.Type.*;
+
+public class RuleSet {
+
+    private final Predicate<Version> rule;
+
+    private RuleSet(Predicate<Version> rule) {
+        this.rule = rule;
+    }
+
+    public boolean isSatisfiedBy(Version version) {
+        return rule.test(version);
+    }
+
+    public static class Builder implements Parser<RuleSet> {
+
+        @Override
+        public RuleSet parse(String input) {
+            Stream<MvnLexer.MvnToken> stream = new MvnLexer().tokenize(input);
+            Predicate<Version> rule = version -> false;
+            while (!stream.positiveLookahead(EOI)) {
+                rule = rule.or(nextRule(stream));
+                if (stream.consume(COMMA, EOI).type == EOI) break;
+                else if(stream.lookahead().type == EOI) throw new UnexpectedTokenException(stream.lookahead(), LEFT_PAREN, LEFT_SQBR);
+            }
+            return new RuleSet(rule);
+        }
+
+        private Predicate<Version> nextRule(Stream<MvnLexer.MvnToken> stream) {
+            MvnLexer.MvnToken l = stream.consume(LEFT_SQBR, LEFT_PAREN);
+            Version v1 = stream.positiveLookahead(NUMERIC) ? parseVersion(stream) : null;
+            Predicate<Version> part1 = v1 == null ? (v) -> true : l.lexeme.equals("[") ? new GreaterOrEqual(v1) : new Greater(v1);
+            stream.consume(MvnLexer.MvnToken.Type.COMMA);
+            Version v2 = stream.positiveLookahead(NUMERIC) ? parseVersion(stream) : null;
+            MvnLexer.MvnToken r = stream.consume(RIGHT_SQBR, RIGHT_PAREN);
+            Predicate<Version> part2 = v2 == null ? (v) -> true : r.lexeme.equals("]") ? new LessOrEqual(v2) : new Less(v2);
+            return part1.and(part2);
+        }
+
+        /**
+         * Parses the {@literal <version>} non-terminal
+         *
+         * @return the parsed version
+         */
+        private Version parseVersion(Stream<MvnLexer.MvnToken> tokens) {
+            int major = Integer.parseInt(tokens.consume(NUMERIC).lexeme);
+            int minor = 0;
+            if (tokens.positiveLookahead(DOT)) {
+                tokens.consume();
+                minor = Integer.parseInt(tokens.consume(NUMERIC).lexeme);
+            }
+            int patch = 0;
+            if (tokens.positiveLookahead(DOT)) {
+                tokens.consume();
+                patch = Integer.parseInt(tokens.consume(NUMERIC).lexeme);
+            }
+            String pre = null;
+            if(tokens.positiveLookahead(HYPHEN)) {
+                tokens.consume();
+                pre = tokens.consume(ALPHA_NUMERIC, NUMERIC).lexeme;
+            }
+            String build = null;
+            if(tokens.positiveLookahead(PLUS)) {
+                tokens.consume();
+                build = tokens.consume(ALPHA_NUMERIC, NUMERIC).lexeme;
+            }
+            Version vtemp = Version.forIntegers(major, minor, patch);
+            if(pre != null) vtemp = vtemp.setPreReleaseVersion(pre);
+            if(build != null) vtemp = vtemp.setBuildMetadata(build);
+            return vtemp;
+        }
+    }
+}
